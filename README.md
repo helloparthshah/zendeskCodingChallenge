41e33275f357700d21947d54537c072b1d3c2779
Parth Shah <helloparthshah@gmail.com>
2021-11-20 14:58:32 -0800

    Updated the readme to have instuctions to connect to the zendesk api

--- README.md
+++ README.md
@@ -4,9 +4,24 @@ A Node.js app using Express.
 
 Make sure you have [Node.js](http://nodejs.org/) installed.
 
+Clone the repo
 ```sh
 $ git clone https://github.com/helloparthshah/zendeskCodingChallenge # or clone your own fork
 $ cd zendeskCodingChallenge
+```
+
+Create a pwd.env file to store your details to connect to the api
+```
+PORT=<Port number>
+ZENDESK_URL=<Your zendesk url>
+EMAIL_ID=<email>
+EMAIL_PASSWORD=<password>
+```
+
+Run the server
+```sh
 $ npm install
 $ npm start
 ```
+
+Access the webpage on localhost
--- app.js
+++ app.js
@@ -81,4 +81,32 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+let port = process.env.PORT || '3000'
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+}).on('error', onError);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ?
+    'Pipe ' + port :
+    'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
